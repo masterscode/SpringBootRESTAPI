81be549d198bb5150f39a11d8f88404e650e6d63
masterscode <ogbinakaemmanuel@gmail.com>
2021-06-17 15:43:46 +0100

    microservices

--- src/main/java/rest/resources/MicroServiceDemo.java
+++ src/main/java/rest/resources/MicroServiceDemo.java
@@ -1,14 +1,26 @@
 package rest.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import rest.entities.microservices.GitProfile;
 
 @RestController
 public class MicroServiceDemo {
-    @GetMapping("/m")
+    @Autowired
+    private WebClient.Builder webClient;
 
+    private final String childService = "localhost:5000";
+
+    @GetMapping("/m")
     public GitProfile getGitProfile(){
-        return null;
+        return webClient
+                .build()
+                .get()
+                .uri(childService +"/webclient")
+                .retrieve()
+                .bodyToMono(GitProfile.class)
+                .block();
     }
 }
